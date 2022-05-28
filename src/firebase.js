82238a6e5cf50445be98a3daaf1dff59bc9e945d
rouclec <senatorasonganyi97@gmail.com>
2022-05-28 14:41:27 +0100

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD65p_zz4_fTxi5PM4e0AL5J5YTigDXOYQ",
+    authDomain: "clone-9ef6f.firebaseapp.com",
+    databaseURL : "https://console.firebase.google.com/project/clone-9ef6f/firestore/data/~2F",
+    projectId: "clone-9ef6f",
+    storageBucket: "clone-9ef6f.appspot.com",
+    messagingSenderId: "436530690038",
+    appId: "1:436530690038:web:4eec23bed5f855a6fb39d7",
+    measurementId: "G-0JFRWN703D"
+  };
